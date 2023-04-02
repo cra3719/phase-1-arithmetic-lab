3c3b2f2faf9ba9b9f44e46e10dbcbc2a2031ae34
Constance <crsampson@gmail.com>
2023-04-02 12:03:08 -0400

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = Number(num1)*Number(num2)
+console.log(multiply)
+
+const random = Number.parseInt(Math.random() *100) + 1
+console.log(random)
+
+const num3=28;
+const num4=6;
+const mod=Number(num3)%Number(num4);
+console.log(mod);
+
+let set = [3, 5, 14, 18, 4, 20, 3]
+let num = ''
+
+let max = Math.max(...set)
+console.log(max)
